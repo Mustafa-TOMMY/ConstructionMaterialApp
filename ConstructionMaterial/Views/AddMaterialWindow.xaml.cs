f2bb9e233554bd0622ba3cf8da920f2976407375
Mustafa-TOMMY <mustafaabdo7199@gmail.com>
2026-03-08 17:28:55 +0200

    Add Material unit enum

--- ConstructionMaterial/Views/AddMaterialWindow.xaml.cs
+++ ConstructionMaterial/Views/AddMaterialWindow.xaml.cs
@@ -13,12 +13,14 @@ namespace ConstructionMaterial.Views
     public partial class AddMaterialWindow : Window
     {
         public List<MaterialType> MaterialTypes { get; }
+        public List<string> MaterialUnits { get; }
         private AppData _data { get; set; }
         public AddMaterialWindow(AppData data)
         {
             InitializeComponent();
             _data = data;
             BtnSave.IsEnabled = false;
+            MaterialUnits = new List<string> { "m³", "m²", "kg", "ton", "Liter" };
             MaterialTypes = Enum.GetValues<MaterialType>().ToList();
             DataContext = this;
         }
@@ -33,13 +35,23 @@ namespace ConstructionMaterial.Views
             MainMaterial material = new MainMaterial()
             {
                 Name = MaterialNameTxt.Text,
-                Unit = UnitTxt.Text,
+                Unit = UnitComboBox.Text,
                 UnitPrice = double.Parse(UnitPriceTxt.Text),
                 Category = (MaterialType)CategoryBox.SelectedItem
             };
-            _data.Materials.Add(material);
-            Helper.SaveToJson(_data);
-            MessageBox.Show("Material added successfully!", "Success", MessageBoxButton.OK);
+            if (!_data.Materials.Any(m => m.Name == MaterialNameTxt.Text))
+            {
+                _data.Materials.Add(material);
+                Helper.SaveToJson(_data);
+                MessageBox.Show("Material added successfully!", "Success", MessageBoxButton.OK);
+            }
+            else
+            {
+                MessageBox.Show("Material with the same name already exists!",
+                    "Error",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
@@ -54,22 +66,13 @@ namespace ConstructionMaterial.Views
                     isValid = double.TryParse(textBox.Text, out double num) && num > 0;
                     errorMessage = "Please enter a valid number";
                 }
-                else if (textBox.Name == "MaterialNameTxt")
-                {
-                    bool isNumber = double.TryParse(textBox.Text, out _);
-                    if (string.IsNullOrWhiteSpace(textBox.Text) || isNumber)
-                    {
-                        isValid = false;
-                        errorMessage = "Please enter a valid material name";
-                    }
-                }
                 else
                 {
                     bool isNumber = double.TryParse(textBox.Text, out _);
                     if (string.IsNullOrWhiteSpace(textBox.Text) || isNumber)
                     {
                         isValid = false;
-                        errorMessage = "Please enter a valid material unit";
+                        errorMessage = "Please enter a valid material name";
                     }
                 }
 
@@ -92,11 +95,10 @@ namespace ConstructionMaterial.Views
         private void TurnOnOffBtn()
         {
             bool isNameValid = !string.IsNullOrWhiteSpace(MaterialNameTxt.Text) && !double.TryParse(MaterialNameTxt.Text, out _);
-            bool isUnitValid = !string.IsNullOrWhiteSpace(UnitTxt.Text) && !double.TryParse(UnitTxt.Text, out _);
             bool isPriceValid = double.TryParse(UnitPriceTxt.Text, out double price) && price > 0;
 
             if (BtnSave != null)
-                BtnSave.IsEnabled = isNameValid && isUnitValid && isPriceValid;
+                BtnSave.IsEnabled = isNameValid && isPriceValid;
         }
 
     }
